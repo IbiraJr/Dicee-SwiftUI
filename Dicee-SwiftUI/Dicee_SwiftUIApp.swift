7ccd67406ec156400d9af8ff2f83a5c923b8f1bb
Ibirá Júnior <ibirajunior98@gmail.com>
2025-05-12 10:09:04 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Ibira Junior on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
